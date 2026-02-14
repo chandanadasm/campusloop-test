966c982edb0ab5cb3aa26ff9a27ea97db004ca96
chandanadas <chandanadasm8590@gmail.com>
2026-02-14 07:17:54 +0530

    debugged landing page

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,18 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Test route
+app.get('/', (req, res) => {
+    res.json({ message: 'CampusLoop API is running' });
+});
 
 // Routes
 app.use('/api/auth', require('./routes/authRoutes'));
